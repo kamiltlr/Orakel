1a3da4c6e1061782f1f1e8b9b5e9fabeb88660d1
kamiltlr <146348629+kamiltlr@users.noreply.github.com>
2024-05-18 00:20:29 +0200

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import MediaSection from './components/MediaSection';
+import GenresSection from './components/GenresSection';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/media" element={<MediaSection />} />
+          <Route path="/genres" element={<GenresSection />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
